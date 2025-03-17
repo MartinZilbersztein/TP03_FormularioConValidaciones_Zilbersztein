867ceb928ef3359cef60a32abd728016ad2a529f
MartinZilber <48520155@est.ort.edu.ar>
2025-03-17 10:38:28 -0300

    Diseño en progreso (aún falta terminar de conectar y programar JavaScript y añadir animaciones y detalles, además del fondo de pantalla

--- codigo.js
+++ codigo.js
@@ -4,15 +4,21 @@ const formulario = document.getElementById('formulario');
 const inputNombre = document.getElementById('nombre');
 const inputEmail = document.getElementById('email');
 let contrasena, contrasena2;
+let nombreValido = false, emailValido = false, contrasenaValida = false, contrasenasCoinciden = false;
 
 inputNombre.onchange = function()
 {
     const feedbackElement = document.getElementById('respuestaNombre');
     if (inputNombre.value.length < 3)
     {
-        feedbackElement.textContent = "El nombre debe incluir tres o más caracteres";
+        feedbackElement.textContent = "El nombre debe incluir tres o más caracteres.";
+        nombreValido = false;
     }
-    else feedbackElement.textContent = "";
+    else 
+    {
+        feedbackElement.textContent = ""; 
+        nombreValido = true
+    }
 }
 inputEmail.onchange = function()
 {
@@ -20,31 +26,51 @@ inputEmail.onchange = function()
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   
     if (emailRegex.test(inputEmail.value)) {
-      feedbackElement.textContent = "";
+      feedbackElement.textContent = ""; emailValido = true;
     } else {
-      feedbackElement.textContent = "Por favor, ingrese un formato de email válido";
+      feedbackElement.textContent = "Por favor, ingrese un formato de email válido.";
+      emailValido = false;
     }
 }
 
 
 inputContrasena.onchange = function()
 {
+    const feedbackElement = document.getElementById('respuestaContrasena');
     contrasena = inputContrasena.value;
     if (contrasena.length < 8)
-    {}
+    {
+        feedbackElement.textContent = "La contraseña debe contener, al menos, ocho caracteres."
+        contrasenaValida = false;
+    }
+    else 
+    {
+        feedbackElement.textContent = "";
+        contrasenaValida = true;
+    }
 }
 
 inputContrasena2.onchange = function()
 {
     contrasena = inputContrasena.value;
     contrasena2 = inputContrasena2.value;
-    const feedbackElement = document.getElementById('respuestaContrasena');
+    const feedbackElement = document.getElementById('respuestaContrasena2');
     if (contrasena != null && contrasena2 != null && contrasena != contrasena2)
-    feedbackElement.textContent = "Las contraseñas no coinciden";
-    else feedbackElement.textContent = "";
+    {
+        feedbackElement.textContent = "Las contraseñas no coinciden.";
+        contrasenasCoinciden = false;
+    }
+    else 
+    {
+        feedbackElement.textContent = "";
+        contrasenasCoinciden = true;
+    }
 }
 
 formulario.addEventListener('submit', function(){
-
+    if (!nombreValido || !emailValido || !contrasenaValida || !contrasenasCoinciden)
+    {
+        event.preventDefault();
+    }
 })
 
