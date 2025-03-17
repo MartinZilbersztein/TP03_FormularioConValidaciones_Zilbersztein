f302fdbe1523ae100d2710ca04667ec7e21baf7c
MartinZilber <48520155@est.ort.edu.ar>
2025-03-17 10:25:29 -0300

    Diseño comenzado

--- codigo.js
+++ codigo.js
@@ -3,11 +3,16 @@ const inputContrasena2 = document.getElementById('contrasena2');
 const formulario = document.getElementById('formulario');
 const inputNombre = document.getElementById('nombre');
 const inputEmail = document.getElementById('email');
+let contrasena, contrasena2;
 
 inputNombre.onchange = function()
 {
+    const feedbackElement = document.getElementById('respuestaNombre');
     if (inputNombre.value.length < 3)
-    console.log("mal");
+    {
+        feedbackElement.textContent = "El nombre debe incluir tres o más caracteres";
+    }
+    else feedbackElement.textContent = "";
 }
 inputEmail.onchange = function()
 {
@@ -20,12 +25,26 @@ inputEmail.onchange = function()
       feedbackElement.textContent = "Por favor, ingrese un formato de email válido";
     }
 }
-inputContraseña2.onchange = function()
+
+
+inputContrasena.onchange = function()
 {
-    const contrasena = inputContrasena.value;
-    const contrasena2 = inputContrasena2.value;
-    const feedbackElement = document.getElementById('respuestaContraseñna');
-    if (contrasena1 != contrasena2)
-    feedbackElement.textContent = "Las contraseñas no coinciden";
+    contrasena = inputContrasena.value;
+    if (contrasena.length < 8)
+    {}
 }
+
+inputContrasena2.onchange = function()
+{
+    contrasena = inputContrasena.value;
+    contrasena2 = inputContrasena2.value;
+    const feedbackElement = document.getElementById('respuestaContrasena');
+    if (contrasena != null && contrasena2 != null && contrasena != contrasena2)
+    feedbackElement.textContent = "Las contraseñas no coinciden";
+    else feedbackElement.textContent = "";
+}
+
+formulario.addEventListener('submit', function(){
+
+})
 
